ea61a948ca58c0f05962593a76488b842d008061
Travis Reeder <treeder@gmail.com>
2018-02-24 13:08:59 -0800

    WIP

--- miner/agent.go
+++ miner/agent.go
@@ -102,6 +102,7 @@ out:
 func (self *CpuAgent) mine(work *Work, stop <-chan struct{}) {
 	if result, err := self.engine.Seal(self.chain, work.Block, stop); result != nil {
 		log.Info("Successfully sealed new block", "number", result.Number(), "hash", result.Hash())
+
 		self.returnCh <- &Result{work, result}
 	} else {
 		if err != nil {
