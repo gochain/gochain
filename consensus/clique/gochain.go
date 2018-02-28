3f2c93ba62cde5b4af2ba0ac860eceeae1fcc50c
Guilherme Rezende <guilhermebr@gmail.com>
2018-02-27 22:01:51 -0300

    clique: fix checkpoint header validation and remove reward
    * clique: fix checkpoint header validation and remove reward
    
    * add signer, signers and voters to ethapi

--- consensus/clique/gochain.go
+++ consensus/clique/gochain.go
@@ -12,25 +12,13 @@ import (
 
 // Finalize implements consensus.Engine, ensuring no uncles are set, but this does give rewards.
 func (c *Clique) Finalize(chain consensus.ChainReader, header *types.Header, state *state.StateDB, txs []*types.Transaction, uncles []*types.Header, receipts []*types.Receipt) (*types.Block, error) {
-	uncles = nil // TODO: or do we want uncles?
-	accumulateRewards(chain.Config(), state, header, uncles)
+	//accumulateRewards(chain.Config(), state, header, uncles)
 	header.Root = state.IntermediateRoot(chain.Config().IsEIP158(header.Number))
-	header.UncleHash = types.CalcUncleHash(uncles)
+	header.UncleHash = types.CalcUncleHash(nil)
 
 	// Assemble and return the final block for sealing
-	return types.NewBlock(header, txs, uncles, receipts), nil
+	return types.NewBlock(header, txs, nil, receipts), nil
 }
-
-// // Finalize implements consensus.Engine, accumulating the block and uncle rewards,
-// // setting the final state and assembling the block.
-// func (ethash *Ethash) Finalize(chain consensus.ChainReader, header *types.Header, state *state.StateDB, txs []*types.Transaction, uncles []*types.Header, receipts []*types.Receipt) (*types.Block, error) {
-// 	// Accumulate any block and uncle rewards and commit the final state root
-// 	accumulateRewards(chain.Config(), state, header, uncles)
-// 	header.Root = state.IntermediateRoot(chain.Config().IsEIP158(header.Number))
-
-// 	// Header seems complete, assemble into a block and return
-// 	return types.NewBlock(header, txs, uncles, receipts), nil
-// }
 
 // Some weird constants to avoid constant memory allocs for them.
 var (
@@ -51,17 +39,15 @@ func accumulateRewards(config *params.ChainConfig, state *state.StateDB, header 
 	// Accumulate the rewards for the miner and any included uncles
 	reward := new(big.Int).Set(blockReward)
 	r := new(big.Int)
-	if uncles != nil {
-		for _, uncle := range uncles {
-			r.Add(uncle.Number, big8)
-			r.Sub(r, header.Number)
-			r.Mul(r, blockReward)
-			r.Div(r, big8)
-			state.AddBalance(uncle.Coinbase, r)
+	for _, uncle := range uncles {
+		r.Add(uncle.Number, big8)
+		r.Sub(r, header.Number)
+		r.Mul(r, blockReward)
+		r.Div(r, big8)
+		state.AddBalance(uncle.Coinbase, r)
 
-			r.Div(blockReward, big32)
-			reward.Add(reward, r)
-		}
+		r.Div(blockReward, big32)
+		reward.Add(reward, r)
 	}
 	log.Info("Issuing", "reward:", reward.String())
 	state.AddBalance(header.Coinbase, reward)
