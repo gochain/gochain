31b42aef0122ceb4b5f54e55d327af5f5fe55029
Travis Reeder <treeder@gmail.com>
2018-03-08 19:18:43 -0500

    Bumps version in code on release.

--- release.sh
+++ release.sh
@@ -14,14 +14,13 @@ else
   exit 1
 fi
 
-version_file="VERSION"
+version_file="params/version.go"
 # Bump version, patch by default - also checks if previous commit message contains `[bump X]`, and if so, bumps the appropriate semver number - https://github.com/treeder/dockers/tree/master/bump
 docker run --rm -it -v $PWD:/app -w /app treeder/bump --filename $version_file "$(git log -1 --pretty=%B)"
 version=$(grep -m1 -Eo "[0-9]+\.[0-9]+\.[0-9]+" $version_file)
 echo "Version: $version"
 
 docker build . -t $user/$image
-
 git add -u
 git commit -m "$image: $version release [skip ci]"
 git tag -f -a "$version" -m "version $version"
