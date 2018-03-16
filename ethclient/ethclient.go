2bab8dd54c827fd6d4356dce5233c2bf3690d645
Travis Reeder <treeder@gmail.com>
2018-03-15 20:35:42 -0700

    Added LatestBlockNumber method to ethclient

--- ethclient/ethclient.go
+++ ethclient/ethclient.go
@@ -70,6 +70,13 @@ func (ec *Client) BlockByNumber(ctx context.Context, number *big.Int) (*types.Bl
 	return ec.getBlock(ctx, "eth_getBlockByNumber", toBlockNumArg(number), true)
 }
 
+// LatestBlockNumber gets latest block number
+func (ec *Client) LatestBlockNumber(ctx context.Context) (*big.Int, error) {
+	var result hexutil.Big
+	err := ec.c.CallContext(ctx, &result, "eth_blockNumber", nil)
+	return (*big.Int)(&result), err
+}
+
 type rpcBlock struct {
 	Hash         common.Hash      `json:"hash"`
 	Transactions []rpcTransaction `json:"transactions"`
