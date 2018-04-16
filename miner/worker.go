39d9023473c7ac410cbdc047c91e574e4a7edede
Jordan Krage <jmank88@gmail.com>
2018-04-16 11:57:10 -0500

    core, ethstats, les, miner: increase chainHeadChanSize (#115)

--- miner/worker.go
+++ miner/worker.go
@@ -44,7 +44,7 @@ const (
 	// The number is referenced from the size of tx pool.
 	txChanSize = 4096
 	// chainHeadChanSize is the size of channel listening to ChainHeadEvent.
-	chainHeadChanSize = 10
+	chainHeadChanSize = 100
 	// chainSideChanSize is the size of channel listening to ChainSideEvent.
 	chainSideChanSize = 10
 )
