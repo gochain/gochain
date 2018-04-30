6468a798f964de460b3da566d95a98fabac0a880
Jordan Krage <jmank88@gmail.com>
2018-04-30 09:31:13 -0500

    core: remove stray account creations in state transition (#16470) (#132)
    The 'from' and 'to' methods on StateTransitions are reader methods and
    shouldn't have inadvertent side effects on state.
    
    It is safe to remove the check in 'from' because account existence is
    implicitly checked by the nonce and balance checks. If the account has
    non-zero balance or nonce, it must exist. Even if the sender account has
    nonce zero at the start of the state transition or no balance, the nonce
    is incremented before execution and the account will be created at that
    time.
    
    It is safe to remove the check in 'to' because the EVM creates the
    account if necessary.
    
    Fixes #15119

--- tests/state_test.go
+++ tests/state_test.go
@@ -39,7 +39,6 @@ func TestState(t *testing.T) {
 	st.fails(`^stRevertTest/RevertPrefoundEmptyOOG\.json/EIP158`, "bug in test")
 	st.fails(`^stRevertTest/RevertPrecompiledTouch\.json/Byzantium`, "bug in test")
 	st.fails(`^stRevertTest/RevertPrefoundEmptyOOG\.json/Byzantium`, "bug in test")
-	st.fails(`^stRandom2/randomStatetest64[45]\.json/(EIP150|Frontier|Homestead)/.*`, "known bug #15119")
 	st.fails(`^stCreateTest/TransactionCollisionToEmpty\.json/EIP158/2`, "known bug ")
 	st.fails(`^stCreateTest/TransactionCollisionToEmpty\.json/EIP158/3`, "known bug ")
 	st.fails(`^stCreateTest/TransactionCollisionToEmpty\.json/Byzantium/2`, "known bug ")
