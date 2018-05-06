04dff8423a8f8d2d36a8abced0ea526ae55e7e5c
jmank88 <jmank88@gmail.com>
2018-05-06 09:53:20 -0500

    consensus/clique: better logging when signed too recently

--- consensus/clique/clique.go
+++ consensus/clique/clique.go
@@ -640,15 +640,17 @@ func (c *Clique) Seal(ctx context.Context, chain consensus.ChainReader, block *t
 		return nil, errUnauthorized
 	}
 	// If we're amongst the recent signers, wait for the next block
+	limit := uint64(len(snap.Signers)/2 + 1)
 	for seen, recent := range snap.Recents {
-		if recent == signer {
-			// Signer is among recents, only wait if the current block doesn't shift it out
-			if limit := uint64(len(snap.Signers)/2 + 1); number < limit || seen > number-limit {
-				log.Info("Signed recently, must wait for others")
-				<-stop
-				return nil, nil
-			}
+		if recent != signer {
+			continue
 		}
+		if number >= seen+limit {
+			continue
+		}
+		log.Info("Signed recently, must wait for others", "number", number, "signed", seen, "next", seen+limit)
+		<-stop
+		return nil, nil
 	}
 	// Sweet, the protocol permits us to sign the block, wait for our time
 	delay := time.Unix(header.Time.Int64(), 0).Sub(time.Now()) // nolint: gosimple
