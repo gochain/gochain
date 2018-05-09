18abaaadaaa608ae8bb514d7c9e3a5f45b7c0969
jmank88 <jmank88@gmail.com>
2018-05-09 11:28:12 -0500

    circle ci: fix

--- release.sh
+++ release.sh
@@ -16,10 +16,10 @@ fi
 
 version_file="params/version.go"
 docker create -v /ver --name file alpine /bin/true
-docker cp $version_file file:/ver
+docker cp $version_file file:/ver/$version_file
 # Bump version, patch by default - also checks if previous commit message contains `[bump X]`, and if so, bumps the appropriate semver number - https://github.com/treeder/dockers/tree/master/bump
 docker run --rm -it --volumes-from file -w /ver treeder/bump --filename $version_file "$(git log -1 --pretty=%B)"
-docker cp file:/ver $version_file
+docker cp file:/ver/$version_file $version_file
 version=$(grep -m1 -Eo "[0-9]+\.[0-9]+\.[0-9]+" $version_file)
 echo "Version: $version"
 
