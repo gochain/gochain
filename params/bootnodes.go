67c08680568f7df9d2291aaa845ac8d04acf546f
jmank88 <jmank88@gmail.com>
2018-05-15 20:07:32 -0500

    params: embed mainnet bootnode enode

--- params/bootnodes.go
+++ params/bootnodes.go
@@ -18,7 +18,9 @@ package params
 
 // MainnetBootnodes are the enode URLs of the P2P bootstrap nodes running on
 // the main Ethereum network.
-var MainnetBootnodes = []string{}
+var MainnetBootnodes = []string{
+	"enode://1f285e244d778fa55198346ee1f126242f380235c37627635e6f9491e8011f63deb25dba8c9e157ad1a27e75df35cf549bbd2c2e103803435bfb181df32ca480@159.65.70.117:30301",
+}
 
 // TestnetBootnodes are the enode URLs of the P2P bootstrap nodes running on the
 // test network.
