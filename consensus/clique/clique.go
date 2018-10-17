7421fd6a2737392f68b94dfe75c11ce1bc769fb9
Jordan Krage <jmank88@gmail.com>
2018-10-17 09:47:56 -0600

    Update consensus/clique/clique.go

--- consensus/clique/clique.go
+++ consensus/clique/clique.go
@@ -613,7 +613,7 @@ func (c *Clique) Prepare(ctx context.Context, chain consensus.ChainReader, heade
 	var deadline *time.Time
 	if c.config.Period != 0 {
 		t := time.Unix(header.Time.Int64(), 0)
-		t = t.Add(-time.Duration(c.config.Period) * time.Second / 3)
+		t = t.Add(-time.Duration(c.config.Period) * time.Second * 2 / 3)
 		deadline = &t
 	}
 	return deadline, nil
