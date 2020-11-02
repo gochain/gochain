d362eb2028acf0bc92da27aff579e848756183e6
jmank88 <jmank88@gmail.com>
2020-11-02 05:56:38 -0600

    add Darvaza fork to burn gas fees

--- core/state_transition.go
+++ core/state_transition.go
@@ -222,7 +222,10 @@ func (st *StateTransition) TransitionDb() (ret []byte, usedGas uint64, failed bo
 		}
 	}
 	st.refundGas()
-	st.state.AddBalance(st.evm.Coinbase, new(big.Int).Mul(new(big.Int).SetUint64(st.gasUsed()), st.gasPrice))
+	fee := new(big.Int).Mul(new(big.Int).SetUint64(st.gasUsed()), st.gasPrice)
+	if !st.evm.ChainConfig().IsDarvaza(st.evm.BlockNumber) {
+		st.state.AddBalance(st.evm.Coinbase, fee)
+	}
 
 	return ret, st.gasUsed(), vmerr != nil, err
 }
